b43ea714c1dc31a6240854d6f8697c1c5ea1ba74
unknown <philipmodino@gmail.com>
2022-09-01 13:22:06 +0300

    Add https

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let weather = {
     "apiKey":"78e8c0db60ab57fda22716aac4e7b629",
     fetchWeather: function (city) {
-        fetch("http://api.openweathermap.org/data/2.5/weather?q="
+        fetch("https://api.openweathermap.org/data/2.5/weather?q="
         +city
         +"&units=metric&APPID="
         +this.apiKey)
